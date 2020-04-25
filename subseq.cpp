06ff30db251200275ff2efce94ba55b16017235d
Guilherme Henrique Moreira <44126014+Guizith@users.noreply.github.com>
2020-04-25 18:00:55 -0300

    Add files via upload

--- subseq.cpp
+++ subseq.cpp
@@ -5,21 +5,25 @@
 
 void subseq(int vet[], int n){
 	int seqs[n];
-	int seqsnum[n][255];
+	int seqsnum[n][254];
 	
 	
 	for(int i=0;i<n;i++){
 		int sub = 0;
-		printf("\n--------------- roda i -----------------\n");
+	//	printf("\n--------------- roda i -----------------\n");
 		int ultimo=0;
+		int idcpst = 0;
 		for(int j=(i+1);j<n;j++){
 			
-			printf("\n comprando %d com %d", vet[j],vet[j-1]);
+		//	printf("\n comprando %d com %d", vet[j],vet[j-1]);
 			//printf("\n valor de j %d valor de n %d", j,n);
 			
 			if(vet[j]> vet[j-1] && vet[j-1]>ultimo){
-				printf("   --- >entrou %d , valor do ultuimo %d", vet[j-1],ultimo);
+			//	printf("   --- >entrou %d , valor do ultuimo %d", vet[j-1],ultimo);
 				sub++;
+				seqsnum[i][idcpst] = vet[j-1];
+				idcpst++;
+				//printf("\n -----------------------------------valor gravado em seqsnum na pos [%d][%d] = %d", i,idcpst,vet[j-1]);
 				ultimo = vet[j-1];
 			}
 			if(j+1 == n){
@@ -27,11 +31,12 @@ void subseq(int vet[], int n){
 					//printf("\n validei ultimo caso");
 					//printf("   --- >entrou last case %d , valor do ultuimo %d, valor de vetj %d", vet[j-1],ultimo,vet[j]);
 					sub++;
+					seqsnum[i][idcpst] = vet[j-1];
+					idcpst++;
+				//	printf("\n -----------------------------------valor gravado em seqsnum na pos [%d][%d] = %d", i,idcpst,vet[j]);
 					ultimo = vet[j];
 				}
-				
 			}
-			
 			
 		}
 		seqs[i] = sub;
@@ -47,13 +52,20 @@ void subseq(int vet[], int n){
 		}	
 	}
 	
+	
+	for(int i=0;i<n;i++){
+		printf("\n");
+		for(int j = 0;j<seqs[idc];j++){
+				printf( "pos [%d][%d] = %d ,",i,j,seqsnum[i][j]);
+		}
+	}
 	/*
-	printf("\n Vetor: ");
+	printf("\n Vetor valor de idc %d: ",idc);
 	for(int i=0;i<seqs[idc];i++){
 		printf( "%d ,", seqsnum[idc][i]);
 
-	}*/
-	
+	}
+	*/
 	printf("\n a maior sequencia eh %d \n",seqs[idc]);
 }
 
